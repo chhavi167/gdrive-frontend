ab0126edd4a145075dcf8cfd1de007327ae381db
chhavi167 <chhavirathi246@gmail.com>
2025-07-01 11:21:12 +0530

    vercel-json

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/api/auth";
+const API_URL = "https://gdrive-backend-17wp.onrender.com/api/auth";
 
 export const registerUser = async (userData: {
   email: string;
